5bfdda751ec79ecab530cd535da530994b13d3ed
Miguel <miguel.giannotti07@outlook.com>
2024-02-21 15:31:09 -0300

    adicionando cors no server

--- src/server.cjs
+++ src/server.cjs
@@ -1,5 +1,19 @@
-const app = require('./app')
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// Configuração do CORS para permitir solicitações de origem específica
+app.use(
+  cors({
+    origin: 'https://juliacakes.vercel.app',
+  }),
+)
+
+// Restante do código do servidor...
 
 const port = process.env.PORT || 3001
 
-app.listen(port, '0.0.0.0')
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
